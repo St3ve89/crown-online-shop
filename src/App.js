cc340467d79b7dd77023805c649ad40e23119a40
Istvan Acs <istvan.acs89@gmail.com>
2019-10-28 13:59:02 +0000

    Created project with create react app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-  return <div> hello</div>;
+  return <HomePage />;
 }
 
 export default App;
