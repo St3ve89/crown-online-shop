82c9bf2b517407a43dac11e68ade649c5e9c3ddc
Istvan Acs <istvan.acs89@gmail.com>
2020-03-30 15:06:25 +0100

    Add hats page for testing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,7 +50,12 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          <MenuItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+            size={size ? size : ''}
+          />
         ))}
       </div>
     );
