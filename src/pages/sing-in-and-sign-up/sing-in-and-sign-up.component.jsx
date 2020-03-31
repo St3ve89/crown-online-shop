dbd246153c6b85431497a632afe8bbbb00b188fe
Istvan Acs <istvan.acs89@gmail.com>
2020-03-31 10:48:09 +0100

    Create sign-in-and-sign-up component

---
+++ src/pages/sing-in-and-sign-up/sing-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sing-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+  <div className='sing-in-and-sign-up'>SIGN IN</div>
+);
+
+export default SignInAndSignUpPage;
