7db280624e38699afd728dddaf264b6b6618c71f
Istvan Acs <istvan.acs89@gmail.com>
2020-03-31 10:58:59 +0100

    Create sign in component

--- src/pages/sing-in-and-sign-up/sing-in-and-sign-up.component.jsx
+++ src/pages/sing-in-and-sign-up/sing-in-and-sign-up.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import './sing-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInAndSignUpPage = () => (
-  <div className='sing-in-and-sign-up'>SIGN IN</div>
+  <div className='sing-in-and-sign-up'>
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUpPage;
