3a8da32a3567fd826d9da4244fe040aabfc13bc1
Istvan Acs <istvan.acs89@gmail.com>
2020-03-31 10:38:59 +0100

    Update pages route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './page/homepage/homepage.component';
-import ShopPage from './page/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 
 function App() {
