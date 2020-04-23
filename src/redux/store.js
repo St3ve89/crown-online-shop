5d4bd15ad3f6af13940e9367683271c1202f9ac0
Istvan Acs <istvan.acs89@gmail.com>
2020-04-23 17:07:23 +0100

    Move redux thunk into saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
