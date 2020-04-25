92aa73403b7ecdb10704d114e39cb071bbd0701b
Istvan Acs <istvan.acs89@gmail.com>
2020-04-25 14:58:12 +0100

    Email sign in into sagas

--- src/App.js
+++ src/App.js
@@ -11,20 +11,15 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import { GlobalStyle } from './global.styles';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
     //   if (userAuth) {
     //     const userRef = await createUserProfileDocument(userAuth);
-
     //     userRef.onSnapshot((snapShot) => {
     //       setCurrentUser({
     //         id: snapShot.id,
@@ -42,6 +37,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log({ user: this.props.currentUser });
     return (
       <div>
         <GlobalStyle />
@@ -67,8 +63,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
